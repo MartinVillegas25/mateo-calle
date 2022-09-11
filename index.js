c6334267bef42f1d132f4218c6e1a436aa907fce
MartinVillegas25 <martinvillegas90@hotmail.com>
2022-09-11 12:44:03 -0300

    mateo calle primer commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//button active-hide
+
+// document.getElementById("home").addEventListener("click", ()=>{
+//     document.getElementById("bio").classList.toggle ("hide");
+// })
+
+//mostrar menu en mobile
+
+// const mobileMenu = document.getElementById('menu');
+// const buttonMenu = document.getElementById('toggle');
+
+
+// buttonMenu.addEventListener("click", ()=>{
+//     mobileMenu.classList.toggle("pages-hide");
+
+// })
+
